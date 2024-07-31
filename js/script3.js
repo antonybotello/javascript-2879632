183555bfb3bf3fac97084a2e674f3a27eda782cb
Antony Botello <antonybotello@gmail.com>
2024-07-30 20:23:03 -0500

    dom

--- js/script3.js
+++ js/script3.js
@@ -1,6 +1,6 @@
 function sumar(){
     var n1= parseInt(document.querySelector("#numero1").value)
-    var n2= parseInt(document.querySelector("#numero2").value)
+    var n2= parseInt(document.getElementById("numero2").value)
     document.querySelector("#resultado").innerHTML=(n1+n2)
     borrar()
 }
@@ -38,4 +38,23 @@ function fiebre(){
         document.querySelector("#resultado2").className="no-fiebre"
 
     }
+
+
+    
+
 }
+const numeros= document.getElementsByClassName("advertencia")
+console.dir(numeros)
+
+const logo= document.createElement("div");
+logo.className="logo tam"
+logo.id="miLogo"
+document.getElementById("inicio").appendChild(logo)
+
+document.getElementById("duplicado").remove()
+
+const miLogo= document.getElementById("miLogo")
+
+miLogo.addEventListener("click",function(){
+    alert("Has hecho click en el logo")
+});
